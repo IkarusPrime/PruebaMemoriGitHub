d905c2f28f215644f3e28a37f85ff7567dab3ea6
IkarusPrime <xxangamaxx@gmail.com>
2022-05-06 12:55:04 +0200

    to do winning panel popup ang generate code for qr

--- script.js
+++ script.js
@@ -1,79 +1,41 @@
 'use strict';
 
-// var cardsArray = [{//array de las cartas
-//   'name': 'shell',
-//   'img': 'img/mario/blueshell.png'
-// }, {
-//   'name': 'star',
-//   'img': 'img/mario/star.png'
-// }, {
-//   'name': 'bobomb',
-//   'img': 'img/mario/bobomb.png'
-// }, {
-//   'name': 'mario',
-//   'img': 'img/mario/mario.png'
-// }, {
-//   'name': 'luigi',
-//   'img': 'img/mario/luigi.png'
-// }, {
-//   'name': 'peach',
-//   'img': 'img/mario/peach.png'
-// }, {
-//   'name': '1up',
-//   'img': 'img/mario/1up.png'
-// }, {
-//   'name': 'mushroom',
-//   'img': 'img/mario/mushroom.png'
-// }, {
-//   'name': 'thwomp',
-//   'img': 'img/mario/thwomp.png'
-// }, {
-//   'name': 'bulletbill',
-//   'img': 'img/mario/bulletbill.png'
-// }, {
-//   'name': 'coin',
-//   'img': 'img/mario/coin.png'
-// }, {
-//   'name': 'goomba',
-//   'img': 'img/mario/goomba.png'
-// }];
-
 var cardsArray = [{//array de las cartas
-  'name': 'pizza',
-  'img': 'img/restaurante/pizza.png'
+  'name': 'postre1',
+  'img': 'img/postre1.jpg'
 }, {
-  'name': 'cafe',
-  'img': 'img/restaurante/cafe.png'
+  'name': 'bolas',
+  'img': 'img/bolas.jpg'
 }, {
-  'name': 'bistec',
-  'img': 'img/restaurante/bistec.png'
+  'name': 'sepia',
+  'img': 'img/sepia.jpg'
 }, {
-  'name': 'ensalada',
-  'img': 'img/restaurante/ensalada.png'
+  'name': 'crema',
+  'img': 'img/crema.jpg'
 }, {
-  'name': 'espageti',
-  'img': 'img/restaurante/espageti.png'
+  'name': 'iogur',
+  'img': 'img/iogur.jpg'
 }, {
-  'name': 'refresco',
-  'img': 'img/restaurante/refresco.png'
+  'name': 'ojos',
+  'img': 'img/ojos.jpg'
 }, {
   'name': 'sopa',
-  'img': 'img/restaurante/sopa.png'
+  'img': 'img/sopa.jpg'
 }, {
-  'name': 'pescado',
-  'img': 'img/restaurante/pescado.png'
+  'name': 'tarta',
+  'img': 'img/tarta.jpg'
 }, {
-  'name': 'vino',
-  'img': 'img/restaurante/vino.png'
+  'name': 'almejas',
+  'img': 'img/almejas.jpg'
 }, {
-  'name': 'bulletbill',
-  'img': 'img/mario/bulletbill.png'
+  'name': 'macarrones',
+  'img': 'img/macarrones.jpg'
 }, {
-  'name': 'coin',
-  'img': 'img/mario/coin.png'
+  'name': 'porcion',
+  'img': 'img/porcion.jpg'
 }, {
-  'name': 'goomba',
-  'img': 'img/mario/goomba.png'
+  'name': 'carne',
+  'img': 'img/carne.jpg'
 }];
 
 var gameGrid = cardsArray.concat(cardsArray).sort(function () {
@@ -87,7 +49,6 @@ var count = 0;//contador para la comparación
 var previousTarget = null;//indica si ha carta en status selected
 var delay = 1200;
 let pairs = 0;
-
 var game = document.getElementById('game');//indica el div del tablero
 var grid = document.createElement('section');//crea un elemento de tipo section para mostrar las cartas
 grid.setAttribute('class', 'grid');//establece el atributo class con valor grid
@@ -96,7 +57,6 @@ game.appendChild(grid);
 gameGrid.forEach(function (item) {
   var name = item.name,//guarda el nombre
       img = item.img;//guarda la foto
-
 
   var card = document.createElement('div');//crea el espacio donda se vera la carta
   card.classList.add('card');//crea la carta
@@ -108,7 +68,6 @@ gameGrid.forEach(function (item) {
   var back = document.createElement('div');//crea el espacio para el reverso de la carta
   back.classList.add('back');//crea el reverso de la carta
   back.style.backgroundImage = 'url(' + img + ')';//assigna la foto al reverso
-
   grid.appendChild(card);//introduce la carta en el objeto
   card.appendChild(front);//introduce la imagen en la cara de la carta
   card.appendChild(back);//introduce la foto del dorso de la carta
@@ -117,11 +76,6 @@ gameGrid.forEach(function (item) {
 let errors = 0, moves = 0, score = 0;
 
 var match = function match() {//pareja correcta
-  score+=5;
-  //añadir visibilidad al contador
-  if (pairs==12) {
-    //victoria, mostrar puntuacion total/premio
-  }
   var selected = document.querySelectorAll('.selected');
   selected.forEach(function (card) {//si la pareja es correcta se le pone el status match para que no se puede seleccionar otra vez
     card.classList.add('match');//sustituye el valor selected por el match en el atributo class
@@ -133,11 +87,6 @@ var resetGuesses = function resetGuesses() {//resetea las variables de las carta
   secondGuess = '';
   count = 0;
   previousTarget = null;
-  errors++;
-  score-=5;
-  if(score<0) score=0;
-  //añadir visibilidad del contador
-
   var selected = document.querySelectorAll('.selected');
   selected.forEach(function (card) {//establece el valor selected en el atributo class
     card.classList.remove('selected');
@@ -145,9 +94,8 @@ var resetGuesses = function resetGuesses() {//resetea las variables de las carta
 };
 
 grid.addEventListener('click', function (event) {//se ejecuta al girar una carta
-
   var clicked = event.target;//se guarda la carta seleccionada
-
+  
   if (clicked.nodeName === 'SECTION' ||//no cartas seleccionadas
       clicked === previousTarget || //previous target no es null
       clicked.parentNode.classList.contains('selected') || //si ha carta selected
@@ -166,12 +114,23 @@ grid.addEventListener('click', function (event) {//se ejecuta al girar una carta
       console.log(secondGuess);
       clicked.parentNode.classList.add('selected');//status selected
       moves++;
-      //añadir vilibilidad al contador;
+      document.getElementById("num1").innerHTML=moves;
     }
 
     if (firstGuess && secondGuess) {//comparación para match
       if (firstGuess === secondGuess) {//hay match
+        score=score+5;
+        document.getElementById("num3").innerHTML=score;
+        if (pairs==12) {
+          //victoria, mostrar puntuacion total/premio
+        }
         setTimeout(match, delay);//desaparecen las cartas
+      }else{
+        errors++;
+        score=score-1;
+        if(score<0) score=0;
+        document.getElementById("num3").innerHTML=score;
+        document.getElementById("num2").innerHTML=errors;
       }
       setTimeout(resetGuesses, delay);//resetea las cartas
     }
